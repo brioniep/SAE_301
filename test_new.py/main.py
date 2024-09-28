3c60e878ea7dd721f30fe090c756e858ee6d7a7f
brioniep <leonard.sero@hotmail.com>
2024-09-28 15:00:48 +0200

    slidetransition

--- test_new.py/main.py
+++ test_new.py/main.py
@@ -5,40 +5,34 @@ from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 
 
 class LoginScreen(Screen):
-
     def on_login(self):
         username = self.ids.username_input.text
         password = self.ids.password_input.text
 
         if username == "leonard" and password == "toto":            
             self.resetForm()
-            self.manager.current = 'success'
-            self.manager.transition = SlideTransition(direction="left")
-
+            self.resetLabel()
+            self.manager.transition = SlideTransition(direction="left")  # Glisse vers la gauche
+            self.manager.current = 'success'  # Changer d'écran
 
         else:
-            self.ids.message_label.text = "Nom d'utilisateur mdp incorrect"
-
-
+            self.ids.message_label.text = "Nom d'utilisateur ou mdp incorrect"
 
     def resetForm(self):
         self.ids.username_input.text = ""
         self.ids.password_input.text = ""
-
-
-
+    
+    def resetLabel(self):
+        self.ids.message_label.text = ""
 
 
 class SuccessScreen(Screen):
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def on_back(self):
-        self.manager.transition = SlideTransition(direction="right")  # Glisse vers la gauche
+        self.manager.transition = SlideTransition(direction="right")  # Glisse vers la droite
         self.manager.current = 'login'  # Revenir à l'écran de connexion
-
-
 
 
 class MyApp(App):
