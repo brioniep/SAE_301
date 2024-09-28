cf72a95862d07512f8282fb26c5579d6a06f35eb
brioniep <leonard.sero@hotmail.com>
2024-09-28 14:22:36 +0200

    ...

--- test_new.py/main.py
+++ test_new.py/main.py
@@ -4,12 +4,21 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen
 
 
-class testApp(Screen) :
+class TestApp(Screen) :
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         pass
 
+    
+    def on_login(self,):
+        username = self.ids.username_input.text
+        password = self.ids.password_input.text
 
+        if username == "leonard" and password =="toto":
+            self.ids.message_label.text = "Vous etes connecté ! "
+        
+        else:
+            self.ids.message_label.text = "Nom d'utilisateur ou mdp incorrect"
 
 
 
@@ -20,7 +29,7 @@ class testApp(Screen) :
 class MyApp(App):
     def build(self):
         
-        return testApp()
+        return TestApp()
     
 if __name__ == '__main__':
     Window.size = (360,640)
