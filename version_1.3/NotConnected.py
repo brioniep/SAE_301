03c125105cd265f8f253762dcf4e8d5725fb9635
Yanis <contact.yaniskorichi@gmail.com>
2024-09-29 16:32:13 +0200

    creation de la branch pour les scripts MQTT utilisable sur django et kivy

--- version_1.3/NotConnected.py
+++
@@ -1,8 +0,0 @@
-from kivy.app import App
-from kivy.uix.screenmanager import Screen, SlideTransition
-
-class NotConnected(Screen):
-    def disconnect(self):
-        self.manager.transition = SlideTransition(direction="left")
-        self.manager.current = 'login'
-        self.manager.get_screen('login').resetForm()
