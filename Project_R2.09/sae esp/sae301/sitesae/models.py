0625d3ba646dc8626273a6377f82a8bd1802c3d6
ReSuKi <97566451+ReSuKiii@users.noreply.github.com>
2024-10-11 11:31:29 +0200

    Update models.py

--- Project_R2.09/sae esp/sae301/sitesae/models.py
+++ Project_R2.09/sae esp/sae301/sitesae/models.py
@@ -5,6 +5,8 @@ class Schedule(models.Model):
     start_time = models.TimeField()  # Heure de début
     end_time = models.TimeField()  # Heure de fin
     prise_id = models.IntegerField()  # ID de la prise
+    email = models.EmailField(default="")  # Adresse email
+    phone = models.CharField(max_length=15, blank=True, default="")
     
 
     def is_active(self):
@@ -14,3 +16,5 @@ class Schedule(models.Model):
 
     def __str__(self):
         return f"Prise {self.prise_id} de {self.start_time} à {self.end_time}"
+
+
