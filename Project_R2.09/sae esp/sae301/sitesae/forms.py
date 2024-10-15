a1d6fcd90ac8a758b64bf2a76de5da6c9cc6046b
ReSuKi <97566451+ReSuKiii@users.noreply.github.com>
2024-10-15 15:14:41 +0200

    Update forms.py

--- Project_R2.09/sae esp/sae301/sitesae/forms.py
+++ Project_R2.09/sae esp/sae301/sitesae/forms.py
@@ -11,13 +11,15 @@ class SitesaeConfig(AppConfig):
 class ScheduleForm(forms.ModelForm):
     email = forms.EmailField(label='Adresse email', required=True)
     phone = forms.CharField(label='Numéro de téléphone', required=False)
+    
 
     class Meta:
         model = Schedule
-        fields = ['start_time', 'end_time', 'prise_id', 'email', 'phone']    
+        fields = ['email', 'phone', 'prise_id', 'start_time', 'end_time', 'temperature_threshold']  
         widgets = {
             'start_time': forms.TimeInput(format='%H:%M'),
             'end_time': forms.TimeInput(format='%H:%M'),
+    
             
         }
     
