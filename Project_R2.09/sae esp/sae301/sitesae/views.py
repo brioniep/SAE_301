63cfcf977026242927aee56f4dcb661f7985f76e
ReSuKi <97566451+ReSuKiii@users.noreply.github.com>
2024-10-16 14:23:10 +0200

    Update views.py

--- Project_R2.09/sae esp/sae301/sitesae/views.py
+++ Project_R2.09/sae esp/sae301/sitesae/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from envoi_MQTT import MQTT  # Importer la classe MQTT
+from envoi_MQTT import MQTT  # la classe est dans le fichier envoi_MQTT.py
 from django.contrib import messages
 from datetime import datetime, time
 from .forms import ScheduleForm 
@@ -14,19 +14,12 @@ from email.mime.multipart import MIMEMultipart
 from twilio.rest import Client
 
 
-# Tmpérature : IUT/température 
-
-# Leds : IUT/on/off/led_1    ( ou _2)
-
-# États des LEDs : IUT/etat/led_1    ( ou _2)
-
-prise_states = {}
-# Initialiser les clients MQTT
-mqtt_client_temperature = MQTT(topic="IUT/temperature")  # Capteur de température
-mqtt_client_led_1 = MQTT(topic="IUT/on/off/led_1")  # LED 1 on/off
-mqtt_client_led_2 = MQTT(topic="IUT/on/off/led_2")  # LED 2 on/off
-mqtt_client_led_1_état = MQTT(topic="IUT/etat/led_1")  # LED 1 etat
-mqtt_client_led_2_état = MQTT(topic="IUT/etat/led_2")  # LED 2 etat
+prise_states = {} # état actuel des prises dans la fonction check_schedule
+mqtt_client_temperature = MQTT(topic="IUT/temperature")  # capteur de température
+mqtt_client_led_1 = MQTT(topic="IUT/on/off/led_1")  # led 1 on/off
+mqtt_client_led_2 = MQTT(topic="IUT/on/off/led_2")  # led 2 on/off
+mqtt_client_led_1_état = MQTT(topic="IUT/etat/led_1")  # led 1 etat
+mqtt_client_led_2_état = MQTT(topic="IUT/etat/led_2")  # led 2 etat
 mqtt_client_temperature.connection()
 mqtt_client_led_1.connection()
 mqtt_client_led_2.connection()
@@ -54,43 +47,41 @@ def index(request):
     if 'user' not in request.session:
         return redirect('sitesae/login')
 
-    # Récupérer tous les plannings
     schedules = Schedule.objects.all()
+    led_1_state = mqtt_client_led_1_état.get_messages()  
+    led_2_state = mqtt_client_led_2_état.get_messages()  
 
-    # Récupérer les états des LEDs depuis le client MQTT
-    led_1_state = mqtt_client_led_1_état.get_messages()  # Récupérer l'état de la LED 1
-    led_2_state = mqtt_client_led_2_état.get_messages()  # Récupérer l'état de la LED 2
-
-    # Passer les états des LEDs au template
+    # passer les états des LEDs au template
     context = {
         'schedules': schedules,
-        'led_1_state': led_1_state or 'off',  # Si pas de message, afficher "off" par défaut
+        'led_1_state': led_1_state or 'off',  # si pas de message, afficher "off" par défaut
         'led_2_state': led_2_state or 'off'
     }
 
     return render(request, 'sitesae/index.html', context)
 
 
-
+# verifier si l'heure actuelle est dans la plage horaire
 def is_within_schedule(start, end):
-    now = datetime.now().time()  # Obtenir l'heure actuelle
+    now = datetime.now().time()  # obtenir l'heure actuelle
     return start <= now <= end
 
+#  recupérer la température
 def get_temperature(request):
-    # Plage horaire autorisée (exemple : entre 8h00 et 20h00)
-    start_time = time(8, 0)  # 08:00 AM
-    end_time = time(20, 0)   # 08:00 PM
+    start_time = time(8, 0) # heure de début de la plage horaire par défaut  
+    end_time = time(20, 0)   
 
     # Vérifier si l'heure actuelle est dans la plage
     if is_within_schedule(start_time, end_time):
-        temperature = mqtt_client_temperature.get_messages()  # Récupérer la température
+        temperature = mqtt_client_temperature.get_messages()  # recup la température
         print("Température reçue :", temperature)  # Debug
         return JsonResponse({'temperature': temperature})
     else:
-        return JsonResponse({'temperature': '--', 'error': 'Temperature display is disabled outside of working hours'})
+        return JsonResponse({'temperature': '--', 'error': 'Temperature display is disabled outside of working hours'}) # en json pour l'affichage (le traitement est fait en js)
 
+
+# envois des messages de base pour allumer et éteindre les prises
 def turn_on(request, prise_id):
-    # Envoyer un message MQTT pour allumer une prise
     if prise_id == 1:
         mqtt_client_led_1.envoi("led_1_on")
         mqtt_client_led_1_état.envoi("led_1_on")
@@ -100,7 +91,6 @@ def turn_on(request, prise_id):
     return redirect('sitesae/index')
 
 def turn_off(request, prise_id):
-    # Envoyer un message MQTT pour éteindre une prise
     if prise_id == 1:
         mqtt_client_led_1.envoi("led_1_off")
         mqtt_client_led_1_état.envoi("led_1_off")
@@ -110,7 +100,6 @@ def turn_off(request, prise_id):
     return redirect('sitesae/index')
 
 def turn_on_all(request):
-    # Envoyer un message MQTT pour allumer les deux prises
     mqtt_client_led_1.envoi("led_1_on")
     mqtt_client_led_2.envoi("led_2_on")
     mqtt_client_led_1_état.envoi("led_1_on")
@@ -118,20 +107,19 @@ def turn_on_all(request):
     return redirect('sitesae/index')
 
 def turn_off_all(request):
-    # Envoyer un message MQTT pour éteindre les deux prises
     mqtt_client_led_1.envoi("led_1_off")
     mqtt_client_led_2.envoi("led_2_off")
     mqtt_client_led_1_état.envoi("led_1_off")
     mqtt_client_led_2_état.envoi("led_2_off")
     return redirect('sitesae/index')
 
-
+# afficher les plages horaires
 def schedule(request):
     if request.method == 'POST':
-        # Logique pour enregistrer les plages horaires
-        pass
+        pass # envoyer les données au formulaire
     return render(request, 'schedule.html')
 
+# connexion de l'utilisateur
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -145,11 +133,13 @@ def login_view(request):
     
     return render(request, 'sitesae/login.html')
 
+# deconnexion de l'utilisateur
 def logout_view(request):
-    request.session.flush()  # Supprime toutes les données de session
+    request.session.flush()  # supprime toutes les données de session
     messages.success(request, "Vous avez été déconnecté.")
     return redirect('sitesae/login')
 
+# vérifier les plannings en arrière-plan et controle les prises
 def check_schedule():
     import time
     while True:
@@ -157,56 +147,52 @@ def check_schedule():
         schedules = Schedule.objects.all()
 
         for schedule in schedules:
-            # Détermine si la prise doit être allumée ou éteinte
+            # vérifier si l'heure actuelle est dans une plage horaire pour la prise donnée
             if schedule.start_time <= current_time <= schedule.end_time:
                 desired_state = 'on'
             else:
                 desired_state = 'off'
             
-            # Récupère l'état actuel de la prise (par défaut 'off')
+            # l'état actuel de la prise 
             current_state = prise_states.get(schedule.prise_id, 'off')
             
-            
-
-            # Si l'état désiré est différent de l'état actuel, envoyer un message MQTT
+            # vérifier si l'état actuel est différent de l'état souhaité
             if schedule.prise_id == 1:
                 if desired_state == 'on':
-                    mqtt_client_led_1.envoi(f"led_{schedule.prise_id}_on")  # Envoyer le message correct pour allumer
+                    mqtt_client_led_1.envoi(f"led_{schedule.prise_id}_on")  
                 else:
-                    mqtt_client_led_1.envoi(f"led_{schedule.prise_id}_off")  # Envoyer le message correct pour éteindre
+                    mqtt_client_led_1.envoi(f"led_{schedule.prise_id}_off")  
             elif schedule.prise_id == 2:
                 if desired_state == 'on':
-                    mqtt_client_led_2.envoi(f"led_{schedule.prise_id}_on")  # Envoyer le message correct pour allumer
+                    mqtt_client_led_2.envoi(f"led_{schedule.prise_id}_on")  
                 else:
-                    mqtt_client_led_2.envoi(f"led_{schedule.prise_id}_off")  # Envoyer le message correct pour éteindre
+                    mqtt_client_led_2.envoi(f"led_{schedule.prise_id}_off")  
                 
-                # Mettre à jour l'état actuel de la prise
-                prise_states[schedule.prise_id] = desired_state
+                prise_states[schedule.prise_id] = desired_state # mettre à jour l'état de la prise
 
-        # Attendre 5 secondes avant de vérifier à nouveau
         time.sleep(5)
 
-# Créer et démarrer un thread qui exécute la vérification en continu
+# demarer un processus en arrière-plan pour vérifier les plannings
 def start_schedule_checker():
     thread = threading.Thread(target=check_schedule, daemon=True)
     thread.start()
 
-# Appeler cette fonction au démarrage de l'application
+# appeler cette fonction au démarrage de l'application
 start_schedule_checker()
 
+# vérifier si une prise doit être allumée ou éteinte
 def check_schedule_for_prise(prise_id):
     current_time = make_aware(datetime.now()).time()
 
-    # Vérifier si l'heure actuelle est dans une plage horaire pour la prise donnée
+    # vérifier si l'heure actuelle est dans une plage horaire pour la prise donnée
     schedules = Schedule.objects.filter(prise_id=prise_id)
     for schedule in schedules:
         if schedule.start_time <= current_time <= schedule.end_time:
-            return True  # Prise doit être allumée
+            return True  # la prise doit être allumée
 
-    return False  # En dehors des plages, prise doit être éteinte
+    return False  # en dehors des plages la prise doit être éteinte
 
-
-
+# logique de vérification des plannings et contrôle des prises, envoi de notifications si nécessaire
 def check_schedules_and_control_prises():
     current_time = make_aware(datetime.now()).time()
 
@@ -230,13 +216,13 @@ def check_schedules_and_control_prises():
         
         current_temperature = float(mqtt_client_temperature.get_messages()) 
 
-        
+    # seuil de température + envoi mail et sms quand dépassé
         if current_temperature > temperature_threshold:
             subject = f"Alerte : Température dépassée pour la prise {prise_id}"
             body = f"La température actuelle est de {current_temperature}°C, dépassant le seuil de {temperature_threshold}°C."
             send_email(user_email, subject, body, to_phone=user_phone)
 
-        # Logique d'allumage/éteignage des prises
+        # logique dallumage/éteignage des prises
         if is_within_schedule and current_state == 'off':
             mqtt_client_led_1.envoi(f"led_{prise_id}_on")
             mqtt_client_led_2.envoi(f"led_{prise_id}_on")
@@ -248,39 +234,40 @@ def check_schedules_and_control_prises():
 
 
 
-
+# pour afficher les plannings
 def schedule_view(request):
     if request.method == 'POST':
         form = ScheduleForm(request.POST)
         if form.is_valid():
-            form.save()  # Save the new schedule
+            form.save()  
             return redirect('sitesae/index')
     else:
-        form = ScheduleForm()
+        form = ScheduleForm() # pour afficher le formulaire vide
 
-    schedules = Schedule.objects.all()  # Retrieve all existing schedules
+    schedules = Schedule.objects.all()  
     return render(request, 'sitesae/index.html', {'form': form, 'schedules': schedules})
 
+# La fonction est plus utilisée mais j'avais la flemme de re changer
 def enforce_schedules(request):
     check_schedules_and_control_prises()
     
     return redirect('sitesae/index')
 
 
-
+# ajout de planning et envoi de notification
 def add_schedule(request):
     if request.method == 'POST':
         form = ScheduleForm(request.POST)
         if form.is_valid():
             schedule = form.save(commit=False)
-            email = form.cleaned_data['email']  # Email de l'utilisateur
-            phone = form.cleaned_data.get('phone')  # Téléphone de l'utilisateur, s'il est présent
+            email = form.cleaned_data['email']  
+            phone = form.cleaned_data.get('phone')  
             schedule.save()
 
             
             subject = "Nouveau planning créé"
             body = f"Un nouveau planning a été créé pour la prise {schedule.prise_id}, de {schedule.start_time} à {schedule.end_time}."
-            send_email(email, subject, body, to_phone=phone)  # Envoi d'email et SMS si le téléphone est fourni
+            send_email(email, subject, body, to_phone=phone)  
 
             messages.success(request, 'Plage horaire ajoutée avec succès !')
             return redirect('sitesae/index')
@@ -295,14 +282,14 @@ def delete_schedule(request, schedule_id):
     if request.method == 'POST':
         # Récupérer le planning à supprimer
         schedule = get_object_or_404(Schedule, id=schedule_id)
-        email = schedule.email  # Email associé au planning
-        phone = schedule.phone  # Numéro de téléphone associé, s'il existe
+        email = schedule.email  
+        phone = schedule.phone  
         schedule.delete()
 
-        # Envoyer la notification après suppression
+        # envoyer la notification après suppression
         subject = "Planning supprimé"
         body = f"Le planning pour la prise {schedule.prise_id} a été supprimé."
-        send_email(email, subject, body, to_phone=phone)
+        send_email(email, subject, body, to_phone=phone) # envoi d'email et SMS si numéro fourni
 
         messages.success(request, "Planning supprimé avec succès !")
     return redirect('sitesae/index')
@@ -310,77 +297,45 @@ def delete_schedule(request, schedule_id):
 
 def delete_all_schedules(request):
     if request.method == 'POST':
-        # Supprimer tous les plannings
+        # supprimer tous les plannings
         Schedule.objects.all().delete()
-        messages.success(request, "Tous les plannings ont été supprimés avec succès !")
+        messages.success(request, "Tous les plannings ont été supprimés avec succès !") 
     return redirect('sitesae/index')
 
 
 def get_led_status(request):
-    # Récupérer les états des LEDs depuis le client MQTT
-    led_1_state = mqtt_client_led_1_état.get_messages()  # Récupérer l'état de la LED 1
-    led_2_state = mqtt_client_led_2_état.get_messages()  # Récupérer l'état de la LED 2
-    
+    led_1_state = mqtt_client_led_1_état.get_messages()  
+    led_2_state = mqtt_client_led_2_état.get_messages()  
 
-    # Retourner les états des LEDs sous forme de JSON
+    # Retourner les états des LEDs sous forme de JSON pour l'affichage
     return JsonResponse({
         'led_1_state': led_1_state or 'off',
         'led_2_state': led_2_state or 'off'
     })
 
-
-# def send_email(to_email, subject, body, to_phone=None):
-#     from_email = "l.petit02400@gmail.com"
-#     from_password = "dfyh xbiv nxft sqdv"
-    
-#     # Configuration du serveur SMTP de Gmail
-#     server = smtplib.SMTP('smtp.gmail.com', 587) #587 for TLS
-#     server.starttls()
-    
-#     try:
-#         # Connexion au compte Gmail
-#         server.login(from_email, from_password)
-
-#         # Création du message
-#         msg = MIMEMultipart()
-#         msg['From'] = from_email
-#         msg['To'] = to_email
-#         msg['Subject'] = subject
-#         msg.attach(MIMEText(body, 'plain'))
-
-#         # Envoi de l'email
-#         server.sendmail(from_email, to_email, msg.as_string())
-#         print(f"Email envoyé avec succès à {to_email}")
-#         if to_phone:
-#             send_sms(to_phone, body)
-#     except Exception as e:
-#         print(f"Erreur lors de l'envoi de l'email: {e}")
-#     finally:
-#         server.quit()
 def send_email(to_email, subject, body, to_phone=None):
     from_email = "l.petit02400@gmail.com"
     from_password = "dfyh xbiv nxft sqdv"
     
-    # Configuration du serveur SMTP de Gmail
-    server = smtplib.SMTP('smtp.gmail.com', 587)  # Port 587 pour TLS
+    server = smtplib.SMTP('smtp.gmail.com', 587)  
     server.starttls()
     
     try:
-        # Connexion au compte Gmail
+        # Connexion
         server.login(from_email, from_password)
 
-        # Création du message
+        # Création 
         msg = MIMEMultipart()
         msg['From'] = from_email
         msg['To'] = to_email
         msg['Subject'] = subject
         msg.attach(MIMEText(body, 'plain'))
 
-        # Envoi de l'email
+        # Envoi 
         server.sendmail(from_email, to_email, msg.as_string())
         print(f"Email envoyé avec succès à {to_email}")
 
-        # Envoi d'un SMS si le numéro de téléphone est fourni
+        # Envoi d'un SMS si numéro fourni
         if to_phone:
             send_sms(to_phone, body)
     
@@ -389,4 +344,3 @@ def send_email(to_email, subject, body, to_phone=None):
     
     finally:
         server.quit()
-
