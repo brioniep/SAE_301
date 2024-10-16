570a9f258ec9097e2d38e12b2b3523f4a849b183
ReSuKi <97566451+ReSuKiii@users.noreply.github.com>
2024-10-16 14:23:33 +0200

    Update models.py

--- Project_R2.09/sae esp/sae301/sitesae/models.py
+++ Project_R2.09/sae esp/sae301/sitesae/models.py
@@ -7,16 +7,15 @@ class Schedule(models.Model):
     end_time = models.TimeField()
     email = models.EmailField(default="")
     phone = models.CharField(max_length=15, blank=True)
-    is_active = models.BooleanField(default=False)
-    temperature_threshold = models.FloatField(null=True, blank=True)
-    
+    is_active = models.BooleanField(default=False) # permet de savoir si la prise est active
+    temperature_threshold = models.FloatField(null=True, blank=True) 
+     
 
-    def is_active(self):
-        """Returns True if the current time is within the schedule range."""
+    def is_active(self): # fonction qui permet de savoir si la prise est active
         now = datetime.now().time()
         return self.start_time <= now <= self.end_time
 
     def __str__(self):
-        return f"Prise {self.prise_id} de {self.start_time} à {self.end_time}"
+        return f"Prise {self.prise_id} de {self.start_time} à {self.end_time}" # affichage de la prise et de l'heure de début et de fin
 
 
